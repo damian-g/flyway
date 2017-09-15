4c2aa28a7c7d04e0729615ab93ce008c953e526a
Damian Garstecki <garstecki.damian@gmail.com>
2017-09-15 11:48:36 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true; do
+	/flyway/flyway info > /dev/null 2>&1;
+
+ 	if [ $? -eq 0 ]; then
+        	break
+    	fi
+    	
+	echo "Database has not been opened yet, trying to reconnect in 10 seconds"
+	sleep 10
+done
+
+sh /flyway/flyway migrate
